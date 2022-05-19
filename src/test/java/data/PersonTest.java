45dc8046902d2f837ffc35f993aebe804c98e43a
Daniel <danimaring16@gmail.com>
2022-05-19 15:50:50 +0200

    Initial commit

---
+++ src/test/java/data/PersonTest.java
@@ -0,0 +1,15 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void getName()
+    {
+        Person p = new Person("James");
+        assertEquals("James", p.getName());
+    }
+}
